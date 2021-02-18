dd9c2427f85ec94ee75ef69addd1a07a1396664f
aq_minato.naka <minato.naka@asia-quest.jp>
2021-02-19 07:50:42 +0900

    全ページルーティング定義追加

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,50 @@
 import { createRouter, createWebHistory } from "vue-router";
+import SignIn from "../views/SignIn.vue";
+import AlbumIndex from "../views/album/Index.vue";
+import AlbumCreate from "../views/album/Create.vue";
+import AlbumEdit from "../views/album/Edit.vue";
+import AlbumShow from "../views/album/Show.vue";
+import PhotoCreate from "../views/photo/Create.vue";
 
-const routes = [];
+const routes = [
+  {
+    path: "/",
+    redirect: { name: "AlbumIndex" },
+  },
+  {
+    path: "/signin",
+    name: "SignIn",
+    component: SignIn,
+  },
+  {
+    path: "/albums",
+    name: "AlbumIndex",
+    component: AlbumIndex,
+  },
+  {
+    path: "/albums/create",
+    name: "AlbumCreate",
+    component: AlbumCreate,
+  },
+  {
+    path: "/albums/:albumId/edit",
+    name: "AlbumEdit",
+    component: AlbumEdit,
+    props: true,
+  },
+  {
+    path: "/albums/:albumId",
+    name: "AlbumShow",
+    component: AlbumShow,
+    props: true,
+  },
+  {
+    path: "/albums/:albumId/photo/create",
+    name: "PhotoCreate",
+    component: PhotoCreate,
+    props: true,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
