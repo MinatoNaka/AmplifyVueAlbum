5772c5059cf4fede05b509a0d74346448c743666
aq_minato.naka <minato.naka@asia-quest.jp>
2021-02-25 08:15:48 +0900

    amplifyパッケージをimport

--- src/main.js
+++ src/main.js
@@ -2,5 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
+import {
+  applyPolyfills,
+  defineCustomElements,
+} from '@aws-amplify/ui-components/loader';
+
+Amplify.configure(aws_exports);
+applyPolyfills().then(() => {
+  defineCustomElements(window);
+});
 
 createApp(App).use(store).use(router).mount('#app')
