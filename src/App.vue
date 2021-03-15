636c902c78fcf8c34a0b8ddd672d5e4633d7ea76
aq_minato.naka <minato.naka@asia-quest.jp>
2021-03-16 08:15:49 +0900

    サインイン時のみサインアウトボタンを表示する制御

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,17 @@
 <template>
-  <amplify-greetings></amplify-greetings>
+    <div v-if="isSignedIn">
+      <amplify-greetings  :username="this.$store.state.user.username"></amplify-greetings>
+    </div>
   <router-view/>
 </template>
+
+<script>
+export default {
+  computed: {
+    isSignedIn() {
+      return this.$store.state.user !== null;
+    },
+  },
+};
+</script>
+
