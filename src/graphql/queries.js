e45b41dd459deac2de2f74bf31ed1bc15a201c90
aq_minato.naka <minato.naka@asia-quest.jp>
2021-03-23 08:20:07 +0900

    amplify pushでqueries,mutations生成

---
+++ src/graphql/queries.js
@@ -0,0 +1,94 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getAlbum = /* GraphQL */ `
+  query GetAlbum($id: ID!) {
+    getAlbum(id: $id) {
+      id
+      name
+      photos {
+        items {
+          id
+          name
+          albumID
+          createdAt
+          updatedAt
+          owner
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const listAlbums = /* GraphQL */ `
+  query ListAlbums(
+    $filter: ModelAlbumFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listAlbums(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        name
+        photos {
+          nextToken
+        }
+        createdAt
+        updatedAt
+        owner
+      }
+      nextToken
+    }
+  }
+`;
+export const getPhoto = /* GraphQL */ `
+  query GetPhoto($id: ID!) {
+    getPhoto(id: $id) {
+      id
+      name
+      albumID
+      album {
+        id
+        name
+        photos {
+          nextToken
+        }
+        createdAt
+        updatedAt
+        owner
+      }
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const listPhotos = /* GraphQL */ `
+  query ListPhotos(
+    $filter: ModelPhotoFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listPhotos(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        name
+        albumID
+        album {
+          id
+          name
+          createdAt
+          updatedAt
+          owner
+        }
+        createdAt
+        updatedAt
+        owner
+      }
+      nextToken
+    }
+  }
+`;
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,135 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateAlbum = /* GraphQL */ `
+  subscription OnCreateAlbum($owner: String!) {
+    onCreateAlbum(owner: $owner) {
+      id
+      name
+      photos {
+        items {
+          id
+          name
+          albumID
+          createdAt
+          updatedAt
+          owner
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const onUpdateAlbum = /* GraphQL */ `
+  subscription OnUpdateAlbum($owner: String!) {
+    onUpdateAlbum(owner: $owner) {
+      id
+      name
+      photos {
+        items {
+          id
+          name
+          albumID
+          createdAt
+          updatedAt
+          owner
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const onDeleteAlbum = /* GraphQL */ `
+  subscription OnDeleteAlbum($owner: String!) {
+    onDeleteAlbum(owner: $owner) {
+      id
+      name
+      photos {
+        items {
+          id
+          name
+          albumID
+          createdAt
+          updatedAt
+          owner
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const onCreatePhoto = /* GraphQL */ `
+  subscription OnCreatePhoto($owner: String!) {
+    onCreatePhoto(owner: $owner) {
+      id
+      name
+      albumID
+      album {
+        id
+        name
+        photos {
+          nextToken
+        }
+        createdAt
+        updatedAt
+        owner
+      }
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const onUpdatePhoto = /* GraphQL */ `
+  subscription OnUpdatePhoto($owner: String!) {
+    onUpdatePhoto(owner: $owner) {
+      id
+      name
+      albumID
+      album {
+        id
+        name
+        photos {
+          nextToken
+        }
+        createdAt
+        updatedAt
+        owner
+      }
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const onDeletePhoto = /* GraphQL */ `
+  subscription OnDeletePhoto($owner: String!) {
+    onDeletePhoto(owner: $owner) {
+      id
+      name
+      albumID
+      album {
+        id
+        name
+        photos {
+          nextToken
+        }
+        createdAt
+        updatedAt
+        owner
+      }
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
