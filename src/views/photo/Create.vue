7d5b25d2e59d76c6cf14c0e60dbd94615b4b748f
aq_minato.naka <minato.naka@asia-quest.jp>
2021-04-08 08:08:14 +0900

    写真登録（名前だけ）実装

--- src/views/photo/Create.vue
+++ src/views/photo/Create.vue
@@ -1,11 +1,49 @@
 <template>
   <div>
     <h1>写真登録</h1>
+    <form @submit.prevent="submitCreate">
+      <label>Name</label><br />
+      <input v-model="form.name" placeholder="Enter photo name" /><br />
+      <p>ここに画像登録フォーム</p>
+      <input type="submit" value="Submit" />
+    </form>
   </div>
 </template>
 
 <script>
+import { API } from "aws-amplify";
+import { createPhoto } from "../../graphql/mutations";
+
 export default {
   name: "PhotoCreate",
+  props: {
+    albumId: String,
+  },
+  data() {
+    return {
+      form: {
+        albumID: this.albumId,
+        name: "",
+      },
+    };
+  },
+  methods: {
+    async submitCreate() {
+      await API.graphql({
+        query: createPhoto,
+        variables: { input: this.form },
+      })
+        .then((result) => {
+          console.log(result);
+          this.$router.push({
+            name: "AlbumShow",
+            params: { albumId: this.albumId },
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
 };
 </script>
